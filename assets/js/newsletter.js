2a30d78062cff9b994b04fca181eb291769a33c8
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-07-05 16:50:23 -0500

    refactored code and moved newsletter js scripts to their own file

---
+++ assets/js/newsletter.js
@@ -0,0 +1,36 @@
+
+  function closeNewsletterPopup() {
+    document.getElementById("newsletter-popup").classList.add("d-none");
+    sessionStorage .setItem("newsletterDismissed", "true");
+  }
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      const dismissed = sessionStorage .getItem("newsletterDismissed");
+      if (!dismissed) {
+        document.getElementById("newsletter-popup").classList.remove("d-none");
+      }
+    }, 5000); // Show after 5 seconds
+  });
+
+
+
+  function handleNewsletterSubmit(event) {
+    event.preventDefault();
+    var form = document.getElementById("newsletterForm");
+    var inputs = form.querySelectorAll("input");
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type !== "hidden" && inputs[i].type !== "checkbox") {
+        inputs[i].value = "";
+      } else if (inputs[i].type === "checkbox") {
+        inputs[i].checked = false;
+      }
+    }
+    document.getElementById("newsletter-success").style.display = "block";
+  }
+
+
+  function openNewsletterPopup() {
+    document.getElementById("newsletter-popup").classList.remove("d-none");
+  }
+
