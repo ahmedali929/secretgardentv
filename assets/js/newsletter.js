48a88557be3f95b83f7c5841744465927c639aa7
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-07-05 17:09:25 -0500

    updated newsletter popup to replace all the text and fields with a thank you message after submitting the info

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -15,22 +15,33 @@
 
 
 
-  function handleNewsletterSubmit(event) {
-    event.preventDefault();
-    var form = document.getElementById("newsletterForm");
-    var inputs = form.querySelectorAll("input");
-    for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].type !== "hidden" && inputs[i].type !== "checkbox") {
-        inputs[i].value = "";
-      } else if (inputs[i].type === "checkbox") {
-        inputs[i].checked = false;
-      }
-    }
-    document.getElementById("newsletter-success").style.display = "block";
-  }
+  // function handleNewsletterSubmit(event) {
+  //   event.preventDefault();
+  //   var form = document.getElementById("newsletterForm");
+  //   var inputs = form.querySelectorAll("input");
+  //   for (var i = 0; i < inputs.length; i++) {
+  //     if (inputs[i].type !== "hidden" && inputs[i].type !== "checkbox") {
+  //       inputs[i].value = "";
+  //     } else if (inputs[i].type === "checkbox") {
+  //       inputs[i].checked = false;
+  //     }
+  //   }
+  //   document.getElementById("newsletter-success").style.display = "block";
+  // }
 
 
   function openNewsletterPopup() {
     document.getElementById("newsletter-popup").classList.remove("d-none");
   }
 
+
+  function handleNewsletterSubmit(event) {
+    event.preventDefault();
+
+    // Hide intro and form
+    document.getElementById("newsletter-intro").style.display = "none";
+    document.getElementById("newsletter-fields").style.display = "none";
+
+    // Show success message
+    document.getElementById("newsletter-success").style.display = "block";
+  }
